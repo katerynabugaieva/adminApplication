590f3dc52e7f1052c5cc923f3963836afed0926c
katerynabugaieva <sonnensmile@gmail.com>
2018-02-13 16:41:16 +0200

    update user template

---
+++ client/admin/js/users/updateUserByAdmin.js
@@ -0,0 +1,84 @@
+Template.updateUserByAdmin.events({
+    'click #checkNumUserUp': function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var data = parseInt($('#userForUpdate').val());
+        var currentLis;
+        currentLis = Workers.findOne({number: data});
+
+        if (currentLis) {
+            $('#isCorrectUserUp').text('запись найдена');
+            $('#divUserUpdate').show();
+        } else {
+            $('#isCorrectUserUp').text('запись не найдена');
+            $('#divUserUpdate').hide();
+        }
+        return false;
+    }
+    ,
+
+    'click #submitUserUp': function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log('in')
+        var data = parseInt($('#userForUpdate').val());
+        var currentLis;
+        currentLis = Workers.findOne({number: data});
+
+        var existData = Workers.findOne({number: $('#iNumUserUp').val()})
+
+        var user = {};
+
+        if (currentLis) {
+            console.log('i am in')
+            if ($('#iNumUserUp').val() !== '') {
+                //  if(existData){
+                //     $('#existData').show()
+                //      console.log(existData)
+                // userSalary.number = parseInt(existData.number);
+                //  }
+                //  else{
+                //      $('#existData').hide();
+                console.log(existData)
+                user.number = parseInt($('#iNumUserUp').val());
+                //  }
+            }
+
+            if($('#iEmailUserUp').val() !== ''){
+                user.email = $('#iEmailUserUp').val();
+            }
+            if($('#iSurnameUserUp').val() !== ''){
+                user.surname = $('#iSurnameUserUp').val();
+        }
+            if($('#iNameUserUp').val() !== ''){
+                user.name = $('#iNameUserUp').val();
+        }
+            if($('#iMiddleNameUserUp').val() !== ''){
+                user.middlename = $('#iMiddleNameUserUp').val();
+        }
+            if($('#iDateOfBirthUp').val() !== ''){
+                user.birthday = $('#iDateOfBirthUp').val();
+        }
+            if($('#iDateStartUp').val() !== ''){
+                user.datestart = $('#iDateStartUp').val();
+            }
+            if($('#iCommentUserUp').val() !== ''){
+                user.comments = $('#iCommentUserUp').val();
+        }
+
+            console.log(user)
+            Workers.update(currentLis._id, {$set: user}, function (error) {
+                if (error) {
+                    alert(error.reason);
+                } else {
+                    console.log('update');
+                }
+            });
+
+
+        }
+        return false;
+    }
+    ,
+
+});
